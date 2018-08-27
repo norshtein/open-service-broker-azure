e724118d4b011571b1199ee87026bc03fbc3d0e1
norshtein <tosi@microsoft.com>
2018-08-27 17:06:32 +0800

    Make the timeout of life cycle test longer

--- tests/lifecycle/test_case_test.go
+++ tests/lifecycle/test_case_test.go
@@ -43,7 +43,7 @@ func (s serviceLifecycleTestCase) execute(
 	catalog service.Catalog,
 	resourceGroup string,
 ) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*40)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*50)
 	defer cancel()
 
 	name := s.getName()
