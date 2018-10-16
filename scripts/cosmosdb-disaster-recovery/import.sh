9ad4462d8be8d60175a445777b4ab038c87609fc
norshtein <tosi@microsoft.com>
2018-10-16 15:32:19 +0800

    Add scripts for DR

---
+++ scripts/cosmosdb-disaster-recovery/import.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+import_file_name=$1
+for row in $(cat ${import_file_name} | jq -c '.[]' ); do
+	resource_group=$(echo $row | jq -r '.resourceGroup')
+	service_name=$(echo $row | jq -r '.serviceName')
+	plan_name=$(echo $row | jq -r '.planName')
+	instance_name=$(echo $row | jq -r '.instanceName')
+	account_name=$(echo $row | jq -r '.accountName')
+	database_name=$(echo $row | jq -r '.databaseName')
+
+	service_name="${service_name}-registered"
+	instance_name="${instance_name}-registered"
+	# echo $service_name  $plan_name $instance_name $account_name $database_name
+	if [ $plan_name != "account" ]; then
+		cf create-service "${service_name}" "${plan_name}" "${instance_name}" -c "{\"resourceGroup\": \"${resource_group}\", \"accountName\": \"${account_name}\", \"databaseName\": \"${database_name}\"}"
+	else
+		cf create-service "${service_name}" "${plan_name}" "${instance_name}" -c "{\"resourceGroup\": \"${resource_group}\", \"accountName\": \"${account_name}\"}"
+	fi
+
+done
